41c09be07a3f4ac9a1f181f2eb4b66899ad6aa6c
Hao Zhou <hzhou@quip.com>
2021-10-17 15:45:09 -0700

    can add new expense

---
+++ react-the-complete-guide/assignment-3/src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2021');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
+    };
+    return (
+        <Card className="expense">
+            <ExpensesFilter select={filteredYear} onChangeFilter={filterChangeHandler}/>
+            {
+                props.items.map(it =>
+                    <ExpenseItem
+                        title={it.title}
+                        amount={it.amount}
+                        date={it.date}
+                    />
+                )
+            }
+        </Card>
+    );
+};
+
+export default Expenses;
