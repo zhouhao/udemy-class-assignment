019a2e23a156902e1b34437ac6efd9a6664e78fc
Hao Zhou <hzhou@quip.com>
2021-10-18 22:37:50 -0700

    add custom UI components

---
+++ react-the-complete-guide/section-11/src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import style from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+    return <div className={style.backdrop} onClick={props.onClose}/>;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={style.modal}>
+            <div className={style.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props)=> {
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </>
+    );
+};
+
+export default Modal;
